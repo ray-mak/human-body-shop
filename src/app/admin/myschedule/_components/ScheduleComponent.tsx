c7208854984317e12861ed9fd5b3eabd633deafc
Ray Mak <projectraymak@gmail.com>
2024-09-12 11:17:10 -0700

    Add appointments to ScheduleComponent Create mobile view and nav for ScheduleComponent

--- src/app/admin/myschedule/_components/ScheduleComponent.tsx
+++ src/app/admin/myschedule/_components/ScheduleComponent.tsx
@@ -3,11 +3,21 @@
 import getAppointments, {
   AppointmentData,
 } from "@/app/actions/appointments/getAppointments"
-import { useAuth } from "@clerk/nextjs"
 import { useEffect, useState } from "react"
 import Calendar from "react-calendar"
 import "../../../../styles/AdminScheduleCalendar.css"
-import { addDays, format, startOfWeek } from "date-fns"
+import {
+  addDays,
+  differenceInMinutes,
+  format,
+  parse,
+  startOfWeek,
+} from "date-fns"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons"
 
 export default function ScheduleComponent() {
   const [appointments, setAppointments] = useState<AppointmentData[]>()
@@ -58,10 +68,30 @@ export default function ScheduleComponent() {
   const daysOfWeek = getDaysOfWeek()
   const hourlyTimes = getHourlyTimes()
 
+  const calculateRowPosition = (startTime: string, endTime: string) => {
+    const startHour = parseInt(startTime.split(":")[0], 10)
+    const startMinutes = parseInt(startTime.split(":")[1], 10)
+
+    const rowStart = startHour + startMinutes / 60
+    const rowSpan =
+      differenceInMinutes(
+        new Date(`1970-01-01T${endTime}:00`),
+        new Date(`1970-01-01T${startTime}:00`)
+      ) / 60
+
+    return { rowStart, rowSpan }
+  }
+
+  function navigateWeek(direction: "next" | "prev") {
+    setSelectedDate((prev) =>
+      direction === "next" ? addDays(prev, 7) : addDays(prev, -7)
+    )
+  }
+
   console.log(appointments, selectedDate)
   return (
-    <div className="w-full p-6 flex gap-4">
-      <div className="w-[280px]">
+    <div className="w-full p-2 lg:p-6 flex flex-col lg:flex-row">
+      <div className="w-full lg:w-[280px] hidden lg:block">
         <Calendar
           view="month"
           calendarType="gregory"
@@ -69,45 +99,104 @@ export default function ScheduleComponent() {
           onClickDay={(date) => setSelectedDate(date)}
         />
       </div>
-      <div className="w-full grid">
-        <div className="grid grid-cols-8">
-          <div className="empty-cell"></div>
-          {daysOfWeek.map((day, index) => (
-            <div
-              key={index}
-              className="text-center py-2 border-l border-gray-300 border-b"
-            >
-              <span className="block font-medium">{format(day, "EEE")}</span>{" "}
-              <span className="block text-sm text-gray-600">
-                {format(day, "MMM d")}
-              </span>{" "}
-            </div>
-          ))}
+      <div>
+        <div className="flex justify-between items-center mb-4 lg:hidden">
+          <button onClick={() => navigateWeek("prev")} className="px-4 py-2">
+            <FontAwesomeIcon icon={faChevronLeft} className="mr-2" />
+            Previous Week
+          </button>
+          <h1 className="text-2xl font-medium">
+            {format(daysOfWeek[0], "MMMM yyyy")}
+          </h1>
+          <button onClick={() => navigateWeek("next")} className="px-4 py-2">
+            Next Week
+            <FontAwesomeIcon icon={faChevronRight} className="ml-2" />
+          </button>
         </div>
-        <div className="grid grid-cols-8">
-          <div className="grid grid-rows-24 grid-cols-2">
-            {hourlyTimes.map((time, index) => (
+      </div>
+      <div className="w-full overflow-x-auto">
+        <div className="grid min-w-[640px] ">
+          <div className="grid grid-cols-8">
+            <div className="empty-cell"></div>
+            {daysOfWeek.map((day, index) => (
               <div
                 key={index}
-                className="text-right h-12 border-gray-300 text-xs col-span-2 grid grid-cols-4"
+                className="text-center py-2 border-l border-gray-300 border-b sticky top-0"
               >
-                <div className="col-span-3 relative">
-                  <p className="absolute -top-2 right-1">{time}</p>
-                </div>
-                <div className="border-t border-gray-300"></div>
+                <span className="block font-medium">{format(day, "EEE")}</span>{" "}
+                <span className="block text-sm text-gray-600">
+                  {format(day, "MMM d")}
+                </span>{" "}
               </div>
             ))}
           </div>
-          {daysOfWeek.map((day, index) => (
-            <div key={index} className="grid grid-rows-24">
+          <div className="grid grid-cols-8 overflow-y-auto max-h-[75vh] relative scrollbar-hide">
+            <div className="grid grid-rows-24 grid-cols-2 sticky left-0">
               {hourlyTimes.map((time, index) => (
                 <div
                   key={index}
-                  className="border-l border-t border-gray-300 h-12 text-xs"
-                ></div>
+                  className="text-right h-12 border-gray-300 text-xs col-span-2 grid grid-cols-4"
+                >
+                  <div className="col-span-3 relative">
+                    <p className="absolute -top-2 right-1">
+                      {index !== 0 ? time : ""}
+                    </p>
+                  </div>
+                  <div className="border-t border-gray-300"></div>
+                </div>
               ))}
             </div>
-          ))}
+
+            {daysOfWeek.map((day, index) => (
+              <div key={index} className="relative grid grid-rows-24">
+                {hourlyTimes.map((time, index) => (
+                  <div
+                    key={index}
+                    className="border-l border-t border-gray-300 h-12 text-xs"
+                  ></div>
+                ))}
+                {appointments &&
+                  appointments
+                    .filter(
+                      (appointment) =>
+                        format(new Date(appointment.date), "EEE MMM d yyyy") ===
+                        format(day, "EEE MMM d yyyy")
+                    )
+                    .map((appointment, index) => {
+                      const { rowStart, rowSpan } = calculateRowPosition(
+                        appointment.startTime,
+                        appointment.endTime
+                      )
+
+                      return (
+                        <div
+                          key={index}
+                          className="absolute w-full bg-blue-500 text-white text-xs p-1 rounded-lg border border-blue-800"
+                          style={{
+                            top: `${rowStart * 3}rem`,
+                            height: `${rowSpan * 3}rem`,
+                          }}
+                        >
+                          <p className="whitespace-nowrap overflow-hidden text-ellipses">
+                            {appointment.serviceType}
+                          </p>
+                          <p className="whitespace-nowrap overflow-hidden text-ellipses">
+                            {format(
+                              parse(appointment.startTime, "HH:mm", new Date()),
+                              "h a"
+                            )}{" "}
+                            -
+                            {format(
+                              parse(appointment.endTime, "HH:mm", new Date()),
+                              "h a"
+                            )}
+                          </p>
+                        </div>
+                      )
+                    })}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- src/app/admin/myschedule/page.tsx
+++ src/app/admin/myschedule/page.tsx
@@ -4,7 +4,6 @@ export default function AdminSchedulePage() {
   return (
     <div className="flex">
       <div className="mt-20 w-full flex flex-col gap-4 items-center justify-center">
-        <h1>My Schedule</h1>
         <ScheduleComponent />
       </div>
     </div>
